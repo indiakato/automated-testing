9edb5b8eff3ba48dbf056ad87912b9286453ec8b
India Kato <india.kato@gmail.com>
2020-09-29 09:18:44 -0700

    constructor, draw method, returns a card, draws 52 cards, returns nil on 53rd draw

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,4 +6,38 @@ describe Deck do
     # 1.  Expected Behavior
     # 2.  Edge cases
 
+  before do
+    @deck = Deck.new
+  end
+
+  describe "constructor" do
+    it "can be instantiated" do
+      expect(@deck).must_be_instance_of Deck
+    end
+  end
+
+  describe "draw method" do
+    it "must respond to draw method" do
+      expect(@deck).must_respond_to :draw
+    end
+
+    it "returns a card" do
+      expect(@deck.draw).must_be_instance_of Card
+    end
+
+    it "will let me draw 52 cards from an initial deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+    end
+
+    it "will return nil for an empty deck" do
+      52.times do
+        expect(@deck.draw).must_be_instance_of Card
+      end
+
+      expect(@deck.draw).must_be_nil
+    end
+  end
+
 end
