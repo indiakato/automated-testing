0e278e2b929bf1d5a4238283570972d6b8c3eef4
India Kato <india.kato@gmail.com>
2020-09-29 09:18:01 -0700

    constructor, draw method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,11 +6,18 @@ require_relative 'card'
 class Deck
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw
     # returns a card
+    drawn_card = @cards.pop
+    return drawn_card
   end
 
   def shuffle
